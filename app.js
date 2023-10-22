84d1de84cd7d45923f3ec105781ecb4fe631e802
Mahakpashine9 <114179154+Mahakpashine9@users.noreply.github.com>
2023-10-22 23:46:13 +0530

    update

--- app.js
+++ app.js
@@ -68,7 +68,22 @@ videoCard.forEach(item => {
     })
 })
 
+// card slider
 
+let cardContainers = [...document.querySelectorAll('.card-container')];
+let preBtns = [...document.querySelectorAll('.pre-btn')];
+let nxtBtns = [...document.querySelectorAll('.nxt-btn')];
 
+cardContainers.forEach((item, i) =>{
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
 
+    nxtBtns[i].addEventListener('click',() =>{
+        item.scrollLeft += containerWidth - 200;
+    })
+
+    preBtns[i].addEventListener('click',() =>{
+        item.scrollLeft -= containerWidth + 200;
+    })
+})
 
